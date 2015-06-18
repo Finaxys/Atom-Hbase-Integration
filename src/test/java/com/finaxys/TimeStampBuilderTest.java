75eaa27d45b1580d6ff5b7e5bbf223e3020794e1
RomanWlm <roman.wilhelm@scaledrisk.com>
2015-06-18 10:08:57 +0200

    Clear

--- src/test/java/com/finaxys/TimeStampBuilderTest.java
+++ src/test/java/com/finaxys/TimeStampBuilderTest.java
@@ -5,132 +5,120 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-import java.util.Date;
-
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 @Test
-public class TimeStampBuilderTest
-{
-    private static final java.util.logging.Logger LOGGER = java.util.logging.Logger.getLogger(HBaseLogger.class.getName());
+public class TimeStampBuilderTest {
+  private static final java.util.logging.Logger LOGGER = java.util.logging.Logger.getLogger(HBaseLogger.class.getName());
 
-    private TimeStampBuilder tsb = new TimeStampBuilder();
+  private TimeStampBuilder tsb = new TimeStampBuilder();
 
-    @BeforeMethod
-    public void setUp() throws Exception
-    {
+  @BeforeMethod
+  public void setUp() throws Exception {
+  }
+
+  @AfterMethod
+  public void tearDown() {
+  }
+
+  public long callNextTimeStampXTime(long nb) {
+    while (nb > 0) {
+      tsb.nextTimeStamp();
+      nb--;
     }
+    return tsb.getTimeStamp();
+  }
 
-    @AfterMethod
-    public void tearDown()
-    {
-    }
-
-    public long callNextTimeStampXTime(long nb)
-    {
-        while (nb > 0)
+  @DataProvider
+  public Object[][] myDataProvider() {
+    return new Object[][]{
         {
-            tsb.nextTimeStamp();
-            nb--;
+            tsb.baseTimeStampForCurrentTick(), tsb.getTimeStamp(), true
+        },
+        {
+            tsb.baseTimeStampForCurrentTick() + tsb.getTimePerOrder(), tsb.nextTimeStamp(), true
+        },
+        {
+            callNextTimeStampXTime(tsb.getNbMaxOrderPerTick()), tsb.baseTimeStampForNextTick(), false
+            //le callnectimestampxtime ne doit pas etre supérieur au basetimestampnextick
         }
-        return tsb.getTimeStamp();
-    }
+    };
+  }
 
-    @DataProvider
-    public Object[][] myDataProvider()
-    {
-        return new Object[][]{
-                {
-                        tsb.baseTimeStampForCurrentTick(), tsb.getTimeStamp(), true
-                },
-                {
-                        tsb.baseTimeStampForCurrentTick() + tsb.getTimePerOrder(), tsb.nextTimeStamp(), true
-                },
-                {
-                        callNextTimeStampXTime(tsb.getNbMaxOrderPerTick()), tsb.baseTimeStampForNextTick(), false
-                        //le callnectimestampxtime ne doit pas etre supérieur au basetimestampnextick
-                }
-        };
-    }
+  @Test(dataProvider = "myDataProvider")
+  public void timestampFor20Ticks(long expected, long received, boolean match) throws Exception {
+    //loadConfig
+    String dateBegin = "09/13/1986";
+    String openHourStr = "9:00";
+    String closeHourStr = "17:30";
+    String nbTickMaxStr = "20";
+    tsb.convertFromString(dateBegin, openHourStr, closeHourStr, nbTickMaxStr);
+    tsb.init();
+    tsb.setTimeStamp(tsb.baseTimeStampForCurrentTick());
+    LOGGER.info("timestamp " + tsb.getTimeStamp());
+    LOGGER.info("bla " + tsb.baseTimeStampForCurrentTick());
+    assertEquals(match, expected == received);
+  }
 
-    @Test(dataProvider = "myDataProvider")
-    public void timestampFor20Ticks(long expected, long received, boolean match) throws Exception
-    {
-        //loadConfig
-        String dateBegin = "09/13/1986";
-        String openHourStr = "9:00";
-        String closeHourStr = "17:30";
-        String nbTickMaxStr = "20";
-        tsb.convertFromString(dateBegin, openHourStr, closeHourStr, nbTickMaxStr);
-        tsb.init();
-        tsb.setTimeStamp(tsb.baseTimeStampForCurrentTick());
-        LOGGER.info("timestamp " + tsb.getTimeStamp());
-        LOGGER.info("bla " + tsb.baseTimeStampForCurrentTick());
-        assertEquals(match, expected == received);
-    }
+  @Test(dataProvider = "myDataProvider")
+  public void timestampFor30kTicks(long expected, long received, boolean match) throws Exception {
+    //loadConfig
+    String dateBegin = "09/13/1986";
+    String openHourStr = "9:00";
+    String closeHourStr = "17:30";
+    String nbTickMaxStr = "30000";
+    tsb.convertFromString(dateBegin, openHourStr, closeHourStr, nbTickMaxStr);
+    tsb.init();
+    tsb.setTimeStamp(tsb.baseTimeStampForCurrentTick());
+    assertEquals(match, expected == received);
+  }
 
-    @Test(dataProvider = "myDataProvider")
-    public void timestampFor30kTicks(long expected, long received, boolean match) throws Exception
-    {
-        //loadConfig
-        String dateBegin = "09/13/1986";
-        String openHourStr = "9:00";
-        String closeHourStr = "17:30";
-        String nbTickMaxStr = "30000";
-        tsb.convertFromString(dateBegin, openHourStr, closeHourStr, nbTickMaxStr);
-        tsb.init();
-        tsb.setTimeStamp(tsb.baseTimeStampForCurrentTick());
-        assertEquals(match, expected == received);
-    }
+  @Test
+  public void timePerOrder() throws Exception {
+    String dateBegin = "09/13/1986";
+    String openHourStr = "9:00";
+    String closeHourStr = "17:30";
+    String nbTickMaxStr = "20";
+    tsb.convertFromString(dateBegin, openHourStr, closeHourStr, nbTickMaxStr);
+    tsb.init();
+    tsb.setTimeStamp(tsb.baseTimeStampForCurrentTick());
+    assertEquals(tsb.baseTimeStampForNextTick() - (tsb.baseTimeStampForCurrentTick()), tsb.getNbMaxOrderPerTick() * (tsb.getTimePerOrder()));
+  }
 
-    @Test
-    public void timePerOrder() throws Exception
-    {
-        String dateBegin = "09/13/1986";
-        String openHourStr = "9:00";
-        String closeHourStr = "17:30";
-        String nbTickMaxStr = "20";
-        tsb.convertFromString(dateBegin, openHourStr, closeHourStr, nbTickMaxStr);
-        tsb.init();
-        tsb.setTimeStamp(tsb.baseTimeStampForCurrentTick());
-        assertEquals(tsb.baseTimeStampForNextTick() - (tsb.baseTimeStampForCurrentTick()), tsb.getNbMaxOrderPerTick() * (tsb.getTimePerOrder()));
-    }
-
-    @Test
-    public void shouldNotByPassBaseTimeStampForNextTick() throws Exception
-    {
-        String dateBegin = "09/13/1986";
-        String openHourStr = "9:00";
-        String closeHourStr = "17:30";
-        String nbTickMaxStr = "20";
-        tsb.convertFromString(dateBegin, openHourStr, closeHourStr, nbTickMaxStr);
-        tsb.init();
-        tsb.setTimeStamp(tsb.baseTimeStampForCurrentTick());
-        LOGGER.info("Next tick = " + tsb.baseTimeStampForNextTick());
-        LOGGER.info("addition = " + (tsb.getTimeStamp() + tsb.getNbMaxOrderPerTick() * (tsb.getTimePerOrder())));
-        LOGGER.info("timeStamp = " + tsb.getTimeStamp());
-        LOGGER.info("add = " + tsb.getNbMaxOrderPerTick() * (tsb.getTimePerOrder()));
-        assertTrue(tsb.baseTimeStampForNextTick() <= (tsb.getTimeStamp() + tsb.getNbMaxOrderPerTick() * (tsb.getTimePerOrder())));
+  @Test
+  public void shouldNotByPassBaseTimeStampForNextTick() throws Exception {
+    String dateBegin = "09/13/1986";
+    String openHourStr = "9:00";
+    String closeHourStr = "17:30";
+    String nbTickMaxStr = "20";
+    tsb.convertFromString(dateBegin, openHourStr, closeHourStr, nbTickMaxStr);
+    tsb.init();
+    tsb.setTimeStamp(tsb.baseTimeStampForCurrentTick());
+    LOGGER.info("Next tick = " + tsb.baseTimeStampForNextTick());
+    LOGGER.info("addition = " + (tsb.getTimeStamp() + tsb.getNbMaxOrderPerTick() * (tsb.getTimePerOrder())));
+    LOGGER.info("timeStamp = " + tsb.getTimeStamp());
+    LOGGER.info("add = " + tsb.getNbMaxOrderPerTick() * (tsb.getTimePerOrder()));
+    assertTrue(tsb.baseTimeStampForNextTick() <= (tsb.getTimeStamp() + tsb.getNbMaxOrderPerTick() * (tsb.getTimePerOrder())));
 
 
-    }
+  }
 
-    @Test
-    public void shouldReachNextTick() throws Exception
-    {
-        TimeStampBuilder t = new TimeStampBuilder("01/01/2000", "10:00", "11:00", "6");
-        t.init();
-        t.setTimeStamp(t.baseTimeStampForCurrentTick());
-        long hour = t.baseTimeStampForNextTick() - t.baseTimeStampForCurrentTick();
-        LOGGER.info("hour = " + hour);
-        assertEquals(600000, t.baseTimeStampForNextTick() - t.baseTimeStampForCurrentTick());
-        hour = t.getNbMaxOrderPerTick() * t.getTimePerOrder();
-        LOGGER.info("hourbis = " + hour);
-        assertEquals(600000, t.getNbMaxOrderPerTick() * t.getTimePerOrder());
-        hour = t.getTimeStamp() + t.getTimePerOrder() * t.getNbMaxOrderPerTick();
-        LOGGER.info("hourbisbis = " + hour);
-        LOGGER.info("nextTick = " + t.baseTimeStampForNextTick());
-        assertEquals(t.getTimeStamp() + t.getTimePerOrder() * t.getNbMaxOrderPerTick(), t.baseTimeStampForNextTick());
-        LOGGER.info("t " + t.getNbMaxOrderPerTick());
-    }
+  @Test
+  public void shouldReachNextTick() throws Exception {
+    TimeStampBuilder t = new TimeStampBuilder("01/01/2000", "10:00", "11:00", "6");
+    t.init();
+    t.setTimeStamp(t.baseTimeStampForCurrentTick());
+    long hour = t.baseTimeStampForNextTick() - t.baseTimeStampForCurrentTick();
+    LOGGER.info("hour = " + hour);
+    assertEquals(600000, t.baseTimeStampForNextTick() - t.baseTimeStampForCurrentTick());
+    hour = t.getNbMaxOrderPerTick() * t.getTimePerOrder();
+    LOGGER.info("hourbis = " + hour);
+    assertEquals(600000, t.getNbMaxOrderPerTick() * t.getTimePerOrder());
+    hour = t.getTimeStamp() + t.getTimePerOrder() * t.getNbMaxOrderPerTick();
+    LOGGER.info("hourbisbis = " + hour);
+    LOGGER.info("nextTick = " + t.baseTimeStampForNextTick());
+    assertEquals(t.getTimeStamp() + t.getTimePerOrder() * t.getNbMaxOrderPerTick(), t.baseTimeStampForNextTick());
+    LOGGER.info("t " + t.getNbMaxOrderPerTick());
+  }
 }
