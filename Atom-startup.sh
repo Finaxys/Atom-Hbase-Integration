5ef1dd13a840d0ba85960303680f814a9ca33656
Mohamed Rafik BRAHIM <raf.brahim@gmail.com>
2016-03-23 18:34:03 +0100

    Update Atom-startup.sh

--- Atom-startup.sh
+++ Atom-startup.sh
@@ -2,10 +2,10 @@
 
 # Atom run server
 cd /tmp/Atom-Hbase-Integration/lib
-runningJar=$(ls Atom-Hbase-Integration*.jar)
+runningJar=$(ls Atom-Hbase-Integration-*.jar)
 
 echo " -- launching Atom CLIENT (NGINX) : nginx"
 nginx
 
 echo " -- launching Atom SERVER : java -jar ${runningJar} Atom-Hbase-Integration"
-java -jar ${runningJar} Atom-Hbase-Integration.Application
+java -jar ${runningJar} org.AHI.Application
